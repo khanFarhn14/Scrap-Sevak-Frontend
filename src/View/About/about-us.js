ee71f40e5efd18bb6374fd274ade2908571131c3
aamir-9987 <aamirmysms@gmail.com>
2025-06-02 16:55:45 +0530

    refactor: about us contet change

--- src/View/About/about-us.js
+++ src/View/About/about-us.js
@@ -24,24 +24,27 @@ export default function About() {
                         <p>Get in Touch With Scrap Sevak" "We're Here to Help" "Connect With Our Team</p>
                     </div>
 
-                    <Row className="g-3" data-aos="fade-up">
+                    <Row className="g-3 align-items-center" data-aos="fade-up">
                         <Col sm={12} md={6} className="order-2 order-md-1">
                             <div className="about-us-content">
                                 <h3>About Scrap Sevak</h3>
                                 <h5>Your Trusted Partner Since Generations</h5>
 
-                                <p>In the busy streets of Mumbai, Gazi Traders has been a name people trust for their scrap needs. For many years,
-                                    families and businesses have chosen us as their reliable partner for selling scrap.</p>
+                                <p>In the fast-paced urban environment of Mumbai, waste disposal is often neglected, leading to environmental degradation and missed 
+                                    opportunities for recycling. At <b>Scrap Sevak</b>, we are revolutionizing scrap and e-waste management by combining eco-conscious 
+                                    practices with digital convenience — making sustainability accessible to everyone, from individual households to large corporate offices.</p>
 
-                                <p>The fact that even government offices trust us with their confidential materials is a testament to the fact that we do not sell
-                                    their confidential materials to food vendors as we do not retail them. <b>The foundation of our work is trust</b>.
-                                    This shows how much people believe in our honest and quality service.</p>
+                                <p>Our platform enables users to schedule scrap pickups with ease — simply download our <b>Scrap Sevak</b> app, register your number, 
+                                    and book a pickup as per your convenience and type of scrap. We collect a wide variety of recyclable materials, including paper, 
+                                    plastic, metals, and electronic waste (e-waste), and ensure responsible disposal through authorized recycling channels.</p>
 
-                                <p>Our skilled team takes care of your scrap with attention, giving you the best value while protecting our environment.
-                                    We use modern methods to make sure our team work helps us all to create a cleaner future.</p>
-
-                                <p>When you pick Scrap Sevak, you get more than just a service. You become part of our family that cares about trust,
-                                    quality, and a cleaner tomorrow.</p>
+                                    <p>We are proud to be a licensed and authorized e-waste buyer, compliant with government norms and certified to handle and recycle 
+                                        electronic waste in a safe, secure, and eco-friendly manner. Whether you’re a resident or a corporate entity, we provide tailored 
+                                        solutions for efficient and lawful waste disposal.</p>
+                                    <p>For our corporate clients, we offer daily scheduled pickups for bulk waste, enabling businesses to meet their ESG goals, 
+                                        CSR commitments, and contribute to a greener India without interrupting operations. Our team of trained professionals ensures 
+                                        timely service, secure handling, and transparent processes at every step.</p>
+                                    <p>By choosing <b>SCRAP SEVAK</b> , you’re not just managing waste — you’re joining a movement towards a cleaner, greener, and smarter India</p>
                             </div>
                         </Col>
                         <Col sm={12} md={6} className="order-1 order-md-2">
